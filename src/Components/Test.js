d432d8bb1a87f1bbdf8a2e16602dc0326194e52b
adrianjose01 <dejesuseusebbio21@hotmail.com>
2024-06-04 22:21:22 -0400

    Adding a button

--- src/Components/Test.js
+++ src/Components/Test.js
@@ -1,7 +1,13 @@
 import React from "react";
+import PrimaryButton from "../UI/PrimaryButton";
 
 const Test = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <PrimaryButton>Este es un boton</PrimaryButton>
+    </>
+  );
 };
 
 export default Test;
