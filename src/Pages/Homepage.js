80402d12ea8f7eed17b64fc950cad538442b2be8
Adrian Jose <dejesuseusebio21@hotmail.com>
2024-06-14 22:58:52 -0400

    Adding steps in the homepage

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -23,6 +23,41 @@ const Homepage = () => {
           Empieza ya
         </Link>
       </div>
+      <section className="my-20">
+        <p className="text-sky-900 font-black text-2xl text-center">
+          Pasos Para Solicitar Tu Permiso
+        </p>
+        <div className="px-3 flex justify-evenly my-10">
+          <article className="border border-sky-900 rounded-lg p-8 flex flex-col items-center gap-4">
+            <p className="bg-sky-900 rounded-full w-10 h-10 flex items-center justify-center text-white font-bold">
+              1
+            </p>
+            <p className="text-xl max-w-24 text-center">Crea una cuenta</p>
+          </article>
+          <article className="border border-sky-900 rounded-lg p-8 flex flex-col items-center gap-4">
+            <p className="bg-sky-900 rounded-full w-10 h-10 flex items-center justify-center text-white font-bold">
+              2
+            </p>
+            <p className="text-xl max-w-24 text-center">
+              Selecciona tu lugar turistico
+            </p>
+          </article>
+          <article className="border border-sky-900 rounded-lg p-8 flex flex-col items-center gap-4">
+            <p className="bg-sky-900 rounded-full w-10 h-10 flex items-center justify-center text-white font-bold">
+              3
+            </p>
+            <p className="text-xl max-w-24 text-center">
+              Llena el formulario de solicitud
+            </p>
+          </article>
+          <article className="border border-sky-900 rounded-lg p-8 flex flex-col items-center gap-4">
+            <p className="bg-sky-900 rounded-full w-10 h-10 flex items-center justify-center text-white font-bold">
+              4
+            </p>
+            <p className="text-xl max-w-24 text-center">Solicita el permiso</p>
+          </article>
+        </div>
+      </section>
       <section className="flex items-center justify-around my-20 px-3">
         <div>
           <img className="w-96" src={imageExperience} />
